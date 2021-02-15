2927d99bc1cf757b48e724d23f99e0484ac8b47f
jgao <aus.jacy@gmail.com>
2021-02-15 22:16:43 +1100

    implemented health display node

---
+++ scenes/HealthDisplay.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var bar_red = preload("res://assets/health_bar/red.png")
+var bar_green = preload("res://assets/health_bar/green.png")
+var bar_yellow = preload("res://assets/health_bar/yellow.png")
+
+onready var healthbar = $HealthBar
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	hide()
+	if get_parent() and get_parent().get("max_health"):
+		healthbar.max_value = get_parent().max_health
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	global_rotation = 0
+
+func update_healthbar(value):
+	healthbar.texture_progress = bar_green
+	if value < healthbar.max_value * 0.7:
+		healthbar.texture_progress = bar_yellow
+	if value < healthbar.max_value * 0.35:
+		healthbar.texture_progress = bar_red
+	if value < healthbar.max_value:
+		show()
+	healthbar.value = value
