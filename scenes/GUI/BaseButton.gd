41e21c3507e2b7e375a18fc3292d3a41afd56fc1
jgao <aus.jacy@gmail.com>
2021-02-27 17:48:42 +1100

    built a generic button for reuse

--- scenes/GUI/BaseButton.gd
+++ scenes/GUI/BaseButton.gd
@@ -3,10 +3,15 @@ extends TextureButton
 onready var time_label = $Counter/Value
 
 export var cooldown = 2.0
+export var counter_text = ""
+export var texture_path = "res://assets/icons/cannon_shot.png"
+export var desciption = ""
 
 signal cannon_fired
 
 func _ready():
+	self.texture_normal = load(texture_path)
+	self.hint_tooltip = desciption
 	$Timer.wait_time = cooldown
 	time_label.hide()
 	$Sweep.texture_progress = texture_normal
