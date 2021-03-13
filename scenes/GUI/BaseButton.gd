9d70cdf750195f762e07248d8d0c8360ce5c8154
jgao <aus.jacy@gmail.com>
2021-03-13 13:20:51 +1100

    added bar menu for main control options

--- scenes/GUI/BaseButton.gd
+++ scenes/GUI/BaseButton.gd
@@ -4,7 +4,7 @@ onready var time_label = $Counter/Value
 
 export var cooldown = 2.0
 export var counter_text = ""
-export var texture_path = "res://assets/icons/cannon_shot.png"
+export var texture_path = "res://assets/icons/click.png"
 export var desciption = ""
 export var cost = 100
 
@@ -12,16 +12,22 @@ signal button_pressed
 
 func _ready():
 	self.texture_normal = load(texture_path)
-	self.hint_tooltip = desciption
+	$Tooltip.text = desciption
 	$Timer.wait_time = cooldown
 	time_label.hide()
-	$Sweep.texture_progress = texture_normal
-	$Sweep.value = 0
+	if cooldown > 0:
+		$Sweep.texture_progress = texture_normal
+		$Sweep.value = 0
+	else: 
+		$Sweep.hide()
+		$Counter.hide()
+	$Tooltip.visible = false
 	set_process(false)
 
 func _process(delta):
 	time_label.text = "%3.1f" % $Timer.time_left
-	$Sweep.value = int(($Timer.time_left / cooldown) * 100)
+	if cooldown > 0:
+		$Sweep.value = int(($Timer.time_left / cooldown) * 100)
 
 func set_cooldown(value):
 	cooldown = value
@@ -33,10 +39,14 @@ func _on_FireButton_pressed():
 	$Timer.start()
 	time_label.show()
 
-
 func _on_Timer_timeout():
-	print("ability ready")
 	$Sweep.value = 0
 	disabled = false
 	time_label.hide()
 	set_process(false)
+
+func _on_BaseButton_mouse_entered():
+	$Tooltip.visible = true
+
+func _on_BaseButton_mouse_exited():
+	$Tooltip.visible = false
