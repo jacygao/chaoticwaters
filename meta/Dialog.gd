a8e6c6c5e12faa206db776e4b209d0555058d43d
jgao <aus.jacy@gmail.com>
2021-03-26 22:14:05 +1100

    created an initial meta file for storing dialog

---
+++ meta/Dialog.gd
@@ -0,0 +1,16 @@
+extends Node
+
+"""
+	This script currently stores metadata for Dialogs.
+"""
+
+var dialog = {
+	0: {
+		"name": "sailor",
+		"text": "Captain, pirate ship spotted at 3 o'clock. Please give order!",
+	},
+	1: {
+		"name": "captain",
+		"text": "Fire.",
+	},
+}
