acf8e73cb3f913f3f97627f90631092ddde0485f
jgao <aus.jacy@gmail.com>
2021-04-07 18:47:06 +1000

    added object popup control node

---
+++ ObjectPopupControl.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+func _ready():
+	close()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	global_rotation = 0
+
+# pos - a Vector2D defining the popup position.
+func open(pos):
+	$ObjectPopupPanel.set_global_position(pos)
+	visible = true
+	
+func close():
+	visible = false
