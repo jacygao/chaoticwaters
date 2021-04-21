2a04d5bdcd0bfe85345e2b453ddb7153bd06d91c
jgao <aus.jacy@gmail.com>
2021-04-21 22:32:30 +1000

    player save system now save occupied city and occupation rate

--- systems/Player.gd
+++ systems/Player.gd
@@ -1,12 +1,23 @@
 extends Node
 
 """
-	The root of the save system storing top level player data
+	The root of the save system storing top level player data.
+	
+	tutorial_on - when set to true helpers appear on the screen.
+	occupation - the rate player occupies every city in key-value format.
+	
 """
 
 var player = {
 	"tutorial_on": true,
+	"occupation": {},
 }
 
 func set_tutorial_off():
 	player["tutorial_on"] = false
+
+func occupy(city, rate):
+	if player["occupation"].has(city):
+		player["occupation"] += rate
+	else:
+		player["occupation"] = rate
